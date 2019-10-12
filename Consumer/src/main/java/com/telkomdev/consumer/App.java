f5e6c07ef1e76d1ae3a29d531d8ae773e4b289be
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-10-12 11:36:25 +0700

    Consumer: print error from wakeup exception

--- Consumer/src/main/java/com/telkomdev/consumer/App.java
+++ Consumer/src/main/java/com/telkomdev/consumer/App.java
@@ -98,7 +98,7 @@ public class App {
             }
 
         } catch (WakeupException ex) {
-
+            System.out.println("Exception caught " + ex.getMessage());
         } finally {
             consumer.close();
         }
