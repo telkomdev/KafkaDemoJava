2859891c197ce9c458ccf709c62ac6a3d1b42e3d
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-10-12 11:21:14 +0700

    Consumer: add custom deserializer product for kafka

---
+++ Consumer/src/main/java/com/telkomdev/consumer/deserializer/ProductAvroDeserializer.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019 wuriyanto.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.telkomdev.consumer.deserializer;
+
+import com.telkomdev.consumer.model.Product;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class ProductAvroDeserializer implements Deserializer<Product> {
+
+    public ProductAvroDeserializer() {
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+
+    }
+
+    @Override
+    public Product deserialize(String s, byte[] data) {
+        try {
+            return data == null ? null : Product.fromAvro(data);
+        } catch (IOException e) {
+            throw new SerializationException("Error when serializing");
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ Consumer/src/main/java/com/telkomdev/consumer/deserializer/ProductProtobufDeserializer.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 wuriyanto.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.telkomdev.consumer.deserializer;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.telkomdev.consumer.model.Product;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+public class ProductProtobufDeserializer implements Deserializer<Product> {
+
+
+    public ProductProtobufDeserializer() {
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+
+    }
+
+    @Override
+    public Product deserialize(String s, byte[] data) {
+        try {
+            return data == null ? null : Product.fromProto(data);
+        } catch (InvalidProtocolBufferException ex) {
+            throw new SerializationException("Error when serializing");
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
